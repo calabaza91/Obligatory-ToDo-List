48a9fe54958bd5ae4a8accdf6532c258e555df24
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-20 09:31:22 -0600

    Refactor some more

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
 //Get input length
-function inputLength(){
-    return input.value.length;
+function inputLength() {
+  return input.value.length;
 };
 
 
 //Create new li element
-function createListElement(){
+function createListElement() {
   var li = document.createElement("li");
   //Create text inside li from textbox
   li.appendChild(document.createTextNode(input.value));
@@ -24,22 +24,24 @@ function createListElement(){
 }
 
 
-//Click button to add more items on list
-button.addEventListener("click", function() {
-
+function addItemAfterClick(){
   //Prevent empty list items with condition statement
   if (inputLength() > 0) {
     createListElement();
   }
-})
+}
 
 
-//Press Enter to add more items on list
-input.addEventListener("keypress", function(event) {
-
+function addItemAfterEnter(event){
   //Prevent empty list items with condition statement
   //And check if Enter was pressed
   if (inputLength() > 0 && event.keyCode === 13) {
     createListElement();
   }
-})
+}
+
+//Click button to add more items on list
+button.addEventListener("click", addItemAfterClick);
+
+//Press Enter to add more items on list
+input.addEventListener("keypress", addItemAfterEnter);
