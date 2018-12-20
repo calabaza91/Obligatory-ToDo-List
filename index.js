9f77fe84fa0648b5c2467e95bc3bc3b64729f4de
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2018-12-20 09:19:51 -0600

    Add keypress event listener

--- index.js
+++ index.js
@@ -4,13 +4,39 @@ var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
 //Click button to add more items on list
-button.addEventListener("click", function(){
+button.addEventListener("click", function() {
 
-  //Create new li element
-  var li = document.createElement("li");
-  //Create text inside li from textbox
-  li.appendChild(document.createTextNode(input.value));
+  //Prevent empty list items with condition statement
+  if (input.value.length > 0) {
+    //Create new li element
+    var li = document.createElement("li");
+    //Create text inside li from textbox
+    li.appendChild(document.createTextNode(input.value));
 
-  //Append li to ul
-  ul.appendChild(li);
+    //Append li to ul
+    ul.appendChild(li);
+
+    //Clear textbox
+    input.value = "";
+  }
+})
+
+
+//Press Enter to add more items on list
+input.addEventListener("keypress", function(event) {
+
+  //Prevent empty list items with condition statement
+  //And check if Enter was pressed
+  if (input.value.length > 0 && event.keyCode === 13) {
+    //Create new li element
+    var li = document.createElement("li");
+    //Create text inside li from textbox
+    li.appendChild(document.createTextNode(input.value));
+
+    //Append li to ul
+    ul.appendChild(li);
+
+    //Clear textbox
+    input.value = "";
+  }
 })
